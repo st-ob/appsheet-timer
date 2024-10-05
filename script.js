d52e17bac78af8ddbc4a337b24f1172c801e2524
st-ob <s.oberleitner@icloud.com>
2024-10-05 11:40:52 +0200

    Update script.js to show workout and break timer

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
+// Get URL parameters
 const urlParams = new URLSearchParams(window.location.search);
-let startSeconds = parseInt(urlParams.get('seconds'), 10);
+let repTimerSeconds = parseInt(urlParams.get('repTimerSeconds'), 10);
+let breakTimerSeconds = parseInt(urlParams.get('breakTimerSeconds'), 10);
 const returnUrl = urlParams.get('returnUrl');
 
-const countdown = () => {
-    if (startSeconds <= 0) {
-        clearInterval(timer);
-        window.location.href = returnUrl;
+const messageElement = document.getElementById('message');
+const timerElement = document.getElementById('timer');
+
+// Function to start the timer
+const startTimer = (duration, message, callback) => {
+    let time = duration;
+    messageElement.innerText = message;
+    const interval = setInterval(() => {
+        timerElement.innerText = time;
+        time--;
+        if (time < 0) {
+            clearInterval(interval);
+            callback();
+        }
+    }, 1000);
+};
+
+// Function to handle the timers
+const handleTimers = () => {
+    if (repTimerSeconds > 0) {
+        startTimer(repTimerSeconds, "Ausführung", () => {
+            if (breakTimerSeconds > 0) {
+                startTimer(breakTimerSeconds, "Pause", () => {
+                    window.location.href = returnUrl;
+                });
+            } else {
+                window.location.href = returnUrl;
+            }
+        });
+    } else if (breakTimerSeconds > 0) {
+        startTimer(breakTimerSeconds, "Pause", () => {
+            window.location.href = returnUrl;
+        });
     } else {
-        document.getElementById('seconds').innerText = startSeconds;
-        startSeconds--;
+        window.location.href = returnUrl;
     }
 };
 
-const timer = setInterval(countdown, 1000);
+// Start the process
+handleTimers();
