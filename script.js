7980cfb9d248eb8b6933cc3a60e6cc0e9810eeac
st-ob <s.oberleitner@icloud.com>
2024-10-05 11:09:45 +0200

    Update script.js
    Include correct website

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search);
 let startSeconds = parseInt(urlParams.get('seconds'), 10);
+const returnUrl = urlParams.get('returnUrl');
 
 const countdown = () => {
     if (startSeconds <= 0) {
         clearInterval(timer);
-        window.location.href = "https://your-appsheet-url.com";
+        window.location.href = returnUrl;
     } else {
         document.getElementById('seconds').innerText = startSeconds;
         startSeconds--;
